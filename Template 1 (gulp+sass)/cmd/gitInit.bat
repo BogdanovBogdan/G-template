2e0d0c1f54aecee79da09320dda5e95559c2d88a
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-07-04 06:17:52 +0300

    update

--- Template 1 (gulp+sass)/cmd/gitInit.bat
+++
@@ -1,9 +0,0 @@
-cd..
-git init
-git add .
-git commit -m "init commit"
-REM git remote add origin *URL* <--- WRITE URL <---
-git push -u origin master
-
-:: that cmd did not close after executing commands
-CMD /Q /K
