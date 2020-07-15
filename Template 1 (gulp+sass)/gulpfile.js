64c910a47cf531b95253772161226f8ed19b0883
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-07-15 23:03:41 +0300

    update gulpfile

--- Template 1 (gulp+sass)/gulpfile.js
+++ Template 1 (gulp+sass)/gulpfile.js
@@ -1,5 +1,6 @@
 "use stirct";
 
+
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
@@ -8,79 +9,132 @@ const terser = require("gulp-terser");
 const concat = require("gulp-concat");
 const cleanCss = require("gulp-clean-css");
 const del = require('del');
+const htmlreplace = require('gulp-html-replace');
 
-const dist = "./docs/";
-// const dist = "D://Program Files//Open Server//OSPanel//domains//";
+
+const prodDist = "./docs/";
+const devDist = "./src/";
+// const serverDest = "D://Program Files//Open Server//OSPanel//domains//";
+
 
 const htmlPath = "./src/index.html";
-function html(done) {
-	return gulp.src(htmlPath)
-		.pipe(gulp.dest(dist))
-		.pipe(browserSync.stream());
-	done();
-};
-
 const cssPath = "./src/css/*.css";
-function css(done) {
-	return gulp.src(cssPath)
-		.pipe(concat('style.min.css'))
-		.pipe(cleanCss())
-		.pipe(gulp.dest(dist + "/css"))
-		.pipe(browserSync.stream());
-	done();
-};
-
 const jsPath = "./src/js/**/script.js";
-function js(done) {
-	return gulp.src(jsPath)
-		.pipe(terser())
-		.pipe(gulp.dest(dist + "/js"))
-		.pipe(browserSync.stream());
-	done();
-};
-
-const copySrcPaths = ["./src/**/*.*", "!./src/sass/**/*.*", "!./src/css/*.css", "!./src/js/*.js"];
-function copySrc(done) {
-	return gulp.src(copySrcPaths)
-		.pipe(gulp.dest(dist))
-		.on("end", browserSync.reload);
-	done();
-};
-
 const sassToCssPath = "./src/sass/**/*.sass";
-function sassToCss(done) {
-	gulp.src(sassToCssPath)
-		.pipe(sass({
-			errorLogToConsole: true
-		}))
-		.on('error', console.error.bind(console))
-		.pipe(autoprefixer({
-			cascade: false,
-			overrideBrowserslist: ["last 3 versions"]
-		}))
-		.pipe(gulp.dest("src/css"))
-		.pipe(browserSync.stream());
+const copySrcPaths = ["./src/**/*.*", `!${htmlPath}`, `!${cssPath}`, `!${jsPath}`, `!${sassToCssPath}`];
+
+
+const buildProd = (done) => {
+
+	function html() {
+		return gulp.src(htmlPath)
+			.pipe(htmlreplace({
+				'stylesheet': './css/style.min.css'
+			}))
+			.pipe(gulp.dest(prodDist));
+	}
+
+	function css() {
+		return gulp.src(cssPath)
+			.pipe(concat('style.min.css'))
+			.pipe(cleanCss())
+			.pipe(gulp.dest(prodDist + "/css"));
+	};
+
+	function js() {
+		return gulp.src(jsPath)
+			.pipe(terser()) // compressor
+			.pipe(gulp.dest(prodDist + "/js"));
+	};
+
+	function copySrc() {
+		return gulp.src(copySrcPaths)
+			.pipe(gulp.dest(prodDist));
+	};
+
+	function cleanDist() {
+		return del(prodDist)
+	}
+
+	(async function run() {
+		browserSync.init({
+			server: prodDist,
+			port: 666
+		});
+
+		await cleanDist();
+		html();
+		css();
+		js();
+		copySrc();
+	})();
+
 	done();
-};
+}
 
-gulp.task("watch", () => {
-	browserSync.init({
-		server: dist,
-		port: 4000,
-		notify: true
-	});
 
-	gulp.watch(htmlPath, html);
-	gulp.watch(sassToCssPath, sassToCss);
-	gulp.watch(cssPath, css);
-	gulp.watch(jsPath, js);
-	gulp.watch(copySrcPaths, copySrc);
-});
+const serve = (done) => {
 
-gulp.task("build", gulp.parallel(html, sassToCss, css, js, copySrc));
-gulp.task("default", gulp.parallel("watch", "build"));
+	function html() {
+		return gulp.src(htmlPath)
+			.pipe(browserSync.stream());
+		done();
+	};
 
+	function css() {
+		return gulp.src(cssPath)
+			.pipe(browserSync.stream());
+		done();
+	};
+
+	function js() {
+		return gulp.src(jsPath)
+			.pipe(browserSync.stream());
+		done();
+	};
+
+	function sassToCss() {
+		gulp.src(sassToCssPath)
+			.pipe(sass({
+				errorLogToConsole: true
+			}))
+			.on('error', console.error.bind(console))
+			.pipe(autoprefixer({
+				cascade: false,
+				overrideBrowserslist: ["last 3 versions"]
+			}))
+			.pipe(gulp.dest("src/css"))
+			.pipe(browserSync.stream());
+		done();
+	};
+
+	function run() {
+		browserSync.init({
+			server: devDist,
+			port: 4000,
+			notify: true
+		});
+
+		html();
+		css();
+		js();
+		sassToCss();
+
+		gulp.watch(htmlPath, html);
+		gulp.watch(cssPath, css);
+		gulp.watch(jsPath, js);
+		gulp.watch(sassToCssPath, sassToCss);
+	}
+	run();
+
+	done();
+}
 
 gulp.task('clean', () => {
 	return del("./**/.gitkeep");
 });
+
+
+gulp.task("buildProd", buildProd)
+gulp.task("default", serve);
+
